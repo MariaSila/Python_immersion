1cc438ddc6ae276079ab22d804e0e3d45b1edacd
Maria <dmn02@mail.ru>
2024-11-02 12:39:14 +0500

    Work at seminar 7

---
+++ Lesson_7/gen_files_to_dir.py
@@ -0,0 +1,46 @@
+# Задание №6
+# ✔ Дорабатываем функции из предыдущих задач.
+# ✔ Генерируйте файлы в указанную директорию — отдельный параметр функции.
+# ✔ Отсутствие/наличие директории не должно вызывать ошибок в работе функции
+# (добавьте проверки).
+# ✔ Существующие файлы не должны удаляться/изменяться в случае совпадения имён.
+
+
+from random import randint, choices
+from string import ascii_lowercase, digits
+from pathlib import Path
+from os import chdir
+
+
+def create_files(
+        extension: str='bin',
+        min_name=6,
+        max_name=30,
+        min_size=256,
+        max_size=4096,
+        num_files=2
+    ) -> None:
+    for _ in range(num_files):
+        while True:
+            name = ''.join(choices(ascii_lowercase + digits + '_', k=randint(min_name, max_name)))
+            name = f'{name}.{extension}'
+            if not Path(name).is_file():
+                break
+        data = bytes(randint(0, 255) for _ in range(randint(min_size, max_size)))
+        with open(name, 'wb') as file:
+            file.write(data)
+
+
+def gen_files(path: str | Path,**kwargs) -> None:
+    if isinstance(path, str):
+        path = Path(path)
+    if not path.is_dir():
+        path.mkdir(parents=True)
+    chdir(path)
+
+    for ext, count in kwargs.items():
+        create_files(extension=ext, num_files=count)
+
+
+if __name__ == '__main__':
+    gen_files(r'C:\Users\user\Python_immersion\Lesson_7\NEW', jpg=3, mp4=2, mkv=1)
---
+++ Lesson_7/write_file_random.py
@@ -0,0 +1,25 @@
+# Задание №1
+# ✔ Напишите функцию, которая заполняет файл
+# (добавляет в конец) случайными парами чисел.
+# ✔ Первое число int, второе - float разделены вертикальной чертой.
+# ✔ Минимальное число - -1000, максимальное - +1000.
+# ✔ Количество строк и имя файла передаются как аргументы функции.
+
+
+import random
+from pathlib import Path
+
+MIN_LIMIT = -1000
+MAX_LIMIT = 1000
+
+
+def write_random_to_file(num_pairs: int, file_name: str | Path) -> None:
+    with open(file_name, 'a', encoding='UTF-8') as file:
+        for _ in range(num_pairs):
+            int_num = random.randint(MIN_LIMIT, MAX_LIMIT)
+            float_num = random.uniform(MIN_LIMIT, MAX_LIMIT)
+            file.write(f'{int_num:>4}|{float_num}\n')
+
+
+if __name__ == '__main__':
+    write_random_to_file(10, 'random_num.txt')
